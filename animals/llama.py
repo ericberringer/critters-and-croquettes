418c357c2d9a4c4abfd33c985c718490733a6e8e
ericberringer <eric.berringer@gmail.com>
2021-04-23 14:47:52 -0500

    Each animal has a name, species, and now a shift property.

---
+++ animals/llama.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+class Llama:
+
+    def __init__(self, name, species, shift):
+        self.name = name
+        self.species = species
+        self.shift = shift
+        self.date_added = date
+        self.walking = True
+
+como_se_llama = Llama("Como Se Llama", "domestic llama", "afternoon")
---
+++ animals/snake.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+class Snake:
+
+    def __init__(self, name, species, shift):
+        self.name = name
+        self.species = species
+        self.shift = shift
+        self.date_added = date
+        self.slithering = True
+
+chris_nake = Snake("Chris Nake", "king snake", "morning")
